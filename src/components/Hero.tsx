7e0550a795458f5dc0139c7977ec8fdeff2dd8a7
Joy Brar <joybrar2001@gmail.com>
2024-07-17 00:31:35 +0530

    Added Transition

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,14 +11,14 @@ const Hero = () => {
       <div className="absolute h-[375px] w-[750px] sm:h-[768px] sm:w-[1536px] lg:h-[1200px] lg:w-[2400px] bg-black rounded-[100%] left-1/2 -translate-x-1/2 border border-[#B58CDE] bg-hero-radial-gradient top-[calc(100%-96px)] sm:top-[calc(100%-120px)]"></div>
 
       <div className="container relative">
-        <div className="flex items-center justify-center">
-          <a href="#" className="inline-flex gap-3 border border-white/30 py-1 px-2 rounded-lg">
+        <div className="flex items-center justify-center group">
+          <a href="#" className="inline-flex gap-3 border border-white/30 py-1 px-2 rounded-lg transition-all group-hover:px-4 group-hover:gap-4">
             <span className="bg-hero-link-gradient text-transparent bg-clip-text [-webkit-background-clip: text]">
               Version 2.0 is here
             </span>
             <span className="inline-flex items-center gap-1">
               <span>Read More</span>
-              <ArrowWIcon />
+              <ArrowWIcon className="group-hover:translate-x-1 group-hover:-rotate-45 transition" />
             </span>
           </a>
         </div>
